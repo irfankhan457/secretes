b17644429a4c2d37f3693e952c153e24e083a854
irfan khan <irfankhan457@gmail.com>
2021-01-15 16:09:20 +0530

    passport cookit and session added

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 //const md5 = require("md5");
 //const encrypt = require("mongoose-encryption");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+//const bcrypt = require("bcrypt");
+//const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportlocalmongoose = require("passport-local-mongoose");
 
 const app = express();
 console.log(process.env.API_KEY);
@@ -16,8 +19,18 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(session({
+    secret: "Our Little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true }) ;
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("useCreateIndex", true);
 
 ///////////////////////////////////// UPDATED CODE FOR ENCRYPTED PASSWORD /////////////////////////////
 const userSchema = new mongoose.Schema({
@@ -25,10 +38,16 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
+userSchema.plugin(passportlocalmongoose);
 //userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
 ///////////////////////////////////// UPDATED CODE FOR ENCRYPTED PASSWORD /////////////////////////////
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
     res.render("home");
@@ -42,43 +61,48 @@ app.get("/register", function(req, res) {
     res.render("register");
 });
 
+app.get("/secrets", function(req, res) {
+    if(req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+})
+
 app.post("/register", function(req, res) {
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        // Store hash in your password DB.
-        const newUser = new User({
-            email:     req.body.username,
-            password:  hash
-        })
-        newUser.save(function(err) {
-            if(err) {
-                res.send("Register Failed")
-                console.log(err);
-            } else {
-                res.render("secrets");
-            }
-        });
-    });
-   
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if(err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            })
+        }
+    })
+  
 });
 
 app.post("/login", function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
+    const user = new User({
+        username: req.body.username,
+        passport: req.body.password
+    });
 
-    User.findOne({email: username}, function(err, foundUser) {
+    req.logIn(user, function(err) {
         if(err) {
-            res.send("login Failed")
             console.log(err);
-        } else if(foundUser) {
-            // Load hash from your password DB.
-            bcrypt.compare(password, foundUser.password, function(err, result) {
-                // result == true
-                if(result === true) {
-                    res.render("secrets");
-                }
-            });
-            }
-        
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            })
+        }
     })
 });
 
